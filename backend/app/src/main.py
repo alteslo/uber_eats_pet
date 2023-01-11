1c45b94a76176404e5b5c8b82b90e0de30ec2d1d
Teslenko Aleksandr <alteslo31@gmail.com>
2023-01-11 20:38:41 +0300

    add main page router

--- backend/app/src/main.py
+++ backend/app/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.src.main_page.router import main_page_router
+from backend.app.src.main_page.router import main_page_router
 
 app = FastAPI()
+
 app.include_router(main_page_router)
--- backend/app/src/main_page/router.py
+++ backend/app/src/main_page/router.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
+from fastapi.staticfiles import StaticFiles
 
 main_page_router = APIRouter()
+
+
+main_page_router.mount(
+    "/", StaticFiles(directory="frontend/html/", html=True), name="index")
