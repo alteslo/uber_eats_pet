16e752cc5bb79ddf56d896ad1fc135026e837100
Teslenko Aleksandr <alteslo31@gmail.com>
2023-01-30 13:50:24 +0300

    del methods

--- backend/app/src/core/exceptions/app_exceptions.py
+++ backend/app/src/core/exceptions/app_exceptions.py
@@ -41,28 +41,7 @@ class ExceptionResult(BaseModel):
     content: ExceptionContent
 
 
-async def get_exception(exc: AppExceptionCase) -> dict:
-    return ExceptionResult(
-        status_code=exc.status_code,
-        message=exc.message,
-        content=ExceptionContent(
-            app_exception=exc.exception_case, context=exc.context),
-    ).dict()
-
-
 class AppException(Exception):
-    class CreateItemError(AppExceptionCase):
-        def __init__(self, message: str = "", context: dict = {}):
-            """
-            Item creation failed
-            """
-            if not message:
-                message = "Item creation failed"
-            status_code = status.HTTP_409_CONFLICT
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
-
     class RouteError(AppExceptionCase):
         def __init__(self, message: str = "", context: dict = {}, status_code=None):
             if not message:
@@ -73,54 +52,6 @@ class AppException(Exception):
                 status_code=status_code,
                 message=traceback.format_exc(),
                 context=context,
-            )
-
-    class GetItemError(AppExceptionCase):
-        def __init__(self, message: str = "", context: dict = {}):
-            """
-            Item not found
-            """
-            if not message:
-                message = "Error occurred while receiving Item"
-            status_code = status.HTTP_404_NOT_FOUND
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
-
-    class GetListError(AppExceptionCase):
-        def __init__(self, message: str = "", context: dict = {}):
-            """
-            Get List error
-            """
-            if not message:
-                message = "Error occurred while receiving List"
-            status_code = status.HTTP_404_NOT_FOUND
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
-
-    class UpdateItemError(AppExceptionCase):
-        def __init__(self, message: str = "", context: dict = {}):
-            """
-            Update Item error
-            """
-            if not message:
-                message = "Element Update Failed"
-            status_code = status.HTTP_404_NOT_FOUND
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
-
-    class DeleteItemError(AppExceptionCase):
-        def __init__(self, message: str = "", context: dict = {}):
-            """
-            Delete Item error
-            """
-            if not message:
-                message = "Element Delete Failed"
-            status_code = status.HTTP_404_NOT_FOUND
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
             )
 
     class ItemRequiresAuth(AppExceptionCase):
@@ -141,17 +72,6 @@ class AppException(Exception):
             if not message:
                 message = "User already exists"
             status_code = status.HTTP_409_CONFLICT
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
-
-    class UserDisabled(AppExceptionCase):
-        """User is disabled"""
-
-        def __init__(self, message: str = "", context: dict = {}):
-            if not message:
-                message = "User is disabled"
-            status_code = status.HTTP_403_FORBIDDEN
             AppExceptionCase.__init__(
                 self, status_code=status_code, message=message, context=context
             )
@@ -178,24 +98,11 @@ class AppException(Exception):
                 self, status_code=status_code, message=message, context=context
             )
 
-    class ItemNotExist(AppExceptionCase):
-        """Item is not exists"""
 
-        def __init__(self, message: str = "", context: dict = {}):
-            if not message:
-                message = "Item is not exists"
-            status_code = status.HTTP_404_NOT_FOUND
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
-
-    class FileError(AppExceptionCase):
-        """File operation error"""
-
-        def __init__(self, message: str = "", context: dict = {}):
-            if not message:
-                message = "File operation error"
-            status_code = status.HTTP_404_NOT_FOUND
-            AppExceptionCase.__init__(
-                self, status_code=status_code, message=message, context=context
-            )
+async def get_exception(exc: AppExceptionCase) -> dict:
+    return ExceptionResult(
+        status_code=exc.status_code,
+        message=exc.message,
+        content=ExceptionContent(
+            app_exception=exc.exception_case, context=exc.context),
+    ).dict()
--- backend/app/src/core/exceptions/service_result.py
+++ backend/app/src/core/exceptions/service_result.py
@@ -1,4 +1,3 @@
-
 import inspect
 
 from fastapi import HTTPException
